45a9920dd4bc5b52a9c308c3e5725fd33ed68529
RichardLechko <richardlechko04@gmail.com>
2024-09-03 18:40:58 -0500

    changed contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://www.richardlechko.com/submit", {
+      const response = await fetch("http://localhost:5000/submit", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
