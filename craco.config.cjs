27c414a18581d234c0a0866da088bd2acfe39fd3
RichardLechko <richardlechko04@gmail.com>
2024-09-03 23:09:26 -0500

    changing craco

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,30 +1,6 @@
 // craco.config.cjs
-const path = require("path");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
-      // Add an alias for '@components'
-      webpackConfig.resolve = {
-        ...webpackConfig.resolve,
-        alias: {
-          ...webpackConfig.resolve.alias,
-          "@components": path.resolve(__dirname, "src/components"),
-        },
-      };
-
-      // Add the BundleAnalyzerPlugin
-      webpackConfig.plugins = [
-        ...webpackConfig.plugins,
-        new BundleAnalyzerPlugin({
-          analyzerMode: "static",
-          reportFilename: "report.html",
-          openAnalyzer: false,
-        }),
-      ];
-
-      return webpackConfig;
-    },
+    configure: (webpackConfig) => webpackConfig,
   },
 };
