f03a3abd75c668dea9bea119a66537ae33cd19a9
RichardLechko <richardlechko04@gmail.com>
2024-09-08 16:12:08 -0500

    updated cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ import weatherRoutes from "./api/weather.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// CORS configuration
+const corsOptions = {
+  origin: "https://www.richardlechko.com", // Your frontend domain
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true, // if you're using cookies/auth
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/currency", currencyRoutes);
