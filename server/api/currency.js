4d8f89e7741d719b3c11819799dad70b08aa56da
RichardLechko <richardlechko04@gmail.com>
2024-09-08 20:29:38 -0500

    vercel.json

--- server/api/currency.js
+++ server/api/currency.js
@@ -8,21 +8,13 @@ config();
 const router = Router();
 
 router.get("/", async (req, res) => {
-  console.log("Received request for /api/currency");
-
   const apiKey = process.env.CURRENCY_API_KEY;
-  console.log("API Key:", apiKey);
-
   const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
-  console.log("API URL:", apiUrl);
-
   try {
     const response = await fetch(apiUrl);
     console.log("Response Status:", response.status);
 
     const data = await response.json();
-    console.log("Response Data:", data);
-
     if (!response.ok) {
       return res.status(response.status).json({ message: data.message });
     }
