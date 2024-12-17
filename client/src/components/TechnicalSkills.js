2e024a4892135990ba3bd984f4efda865d8e8562
RichardLechko <richardlechko04@gmail.com>
2024-12-16 18:54:36 -0600

    improved skills

--- client/src/components/TechnicalSkills.js
+++ client/src/components/TechnicalSkills.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import { DiReact, DiNodejs, DiGithubBadge } from "react-icons/di";
 import { BiLogoRedux, BiLogoGit, BiLogoAws } from "react-icons/bi";
 import {
